964566e4c8268226b96db33f0d54e17cba9b85f7
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-10-02 15:27:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ numbers = [    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                [0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
                [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]]
 
-rnum = len(numbers)
-cnum = len(numbers[0])
+cnt = 0
+for i in range(1, len(numbers)-1):
+	for j in range(1, len(numbers[0])-1):
+		if numbers[i][j] and numbers[i+1][j] and numbers[i-1][j] and numbers[i][j-1] and numbers[i][j+1]:
+			cnt += 1
 
-# ******************************
-# Make your Code
-# ******************************
+print(cnt)
+
